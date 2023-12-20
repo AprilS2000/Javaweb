6edc558b6eb76eb6af0f19cd99af27937187f338
april <aprilshen0401@gmail.com>
2023-12-20 12:03:20 +0800

    up 1203

--- GroupBuy/src/main/java/spring/mvc/group_buy/controller/GroupBuyController.java
+++ GroupBuy/src/main/java/spring/mvc/group_buy/controller/GroupBuyController.java
@@ -1,17 +1,64 @@
 package spring.mvc.group_buy.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import spring.mvc.group_buy.model.dao.GroupBuyDao;
+import spring.mvc.group_buy.model.entity.Product;
+import spring.mvc.group_buy.model.entity.User;
 
 @Controller
 @RequestMapping("/group_buy")
 public class GroupBuyController {
 
+	private GroupBuyDao dao;
+	
 	//登入首頁
 	@GetMapping(value = {"/login", "/", "/login/"})
 	public String loginPage() {
 		return "group_buy/login"; 
 	}
+	
+	@PostMapping("./login")
+	public String login(@RequestParam("username") String username,
+						@RequestParam("passwrod") String password,
+						HttpSession session, Model model) {
+		// 根據 username 查找 user 物件
+				Optional<User> userOpt = dao.findUserByUsername(username);
+				if(userOpt.isPresent()) {
+					User user = userOpt.get();
+					// 比對 password
+					if(user.getPassword().equals(password)) {
+						session.setAttribute("user", user); // 將 user 物件放入到 session 變數中
+						return "redirect:/mvc/group_buy/frontend/main"; // OK, 導向前台首頁
+					} else {
+						model.addAttribute("loginMessage", "密碼錯誤");
+						return "group_buy/login";
+					}
+				} else {
+					model.addAttribute("loginMessage", "無此使用者");
+					return "group_buy/login";
+				}
+	}
+	
+		// 前台團購首頁
+	@RequestMapping("/frontend/main")
+	@ResponseBody
+	public String frontendMain(Model model) {
+		List<Product> products = dao.findAllProducts();
+		model.addAttribute("products", products);
+		
+		return "group_buy/frontend/main";
+	}
+			
 }
--- GroupBuy/src/main/java/spring/mvc/group_buy/model/dao/GroupBuyDaoMySQL.java
+++ GroupBuy/src/main/java/spring/mvc/group_buy/model/dao/GroupBuyDaoMySQL.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import spring.mvc.group_buy.model.entity.Cart;
@@ -14,34 +16,48 @@ import spring.mvc.group_buy.model.entity.User;
 @Repository
 public class GroupBuyDaoMySQL implements GroupBuyDao {
 
+	private JdbcTemplate jdbcTemplate;
+	
 	@Override
 	public List<User> findAllUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select userId, username, password, level from user";
+		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
 	}
 
 	@Override
 	public void addUser(User user) {
-		// TODO Auto-generated method stub
+		String sql = "insert into user(username, password, level) values(?, ?, ?)";
+		jdbcTemplate.update(sql, user.getUsername(), user.getPassword(), user.getLevel());
 		
 	}
 
 	@Override
 	public Boolean updateUserPassword(Integer userId, String newPassword) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "update user set password = ? where userId = ?";
+		int rowcount = jdbcTemplate.update(sql, newPassword, userId);
+		return rowcount > 0;
 	}
 
 	@Override
 	public Optional<User> findUserByUsername(String username) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		String sql = "select userId, username, password, level from user where username = ?";
+		try {
+			User user = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(User.class), username);
+			return Optional.ofNullable(user);
+		} catch (Exception e) {
+			return Optional.empty();
+		}
 	}
 
 	@Override
 	public Optional<User> findUserById(Integer userId) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		String sql = "select userId, username, password, level from user where userId = ?";
+		try {
+			User user = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(User.class), userId);
+			return Optional.ofNullable(user);
+		} catch (Exception e) {
+			return Optional.empty();
+		}
 	}
 
 	@Override
